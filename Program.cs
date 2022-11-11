9dc884a00850215773a35a301976f7e67bfd25f9
momo <mohamedndao90@gmail.com>
2022-11-11 22:08:49 +0000

    Exercice conversion hexa

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿int dec, rem;
+char [] hexchars = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F' };
+String hex = "";
+Console.Write("Entrez un entier ");
+dec = int.Parse(Console.ReadLine());
+
+
+
+while (dec > 0)
+{
+    rem = dec % 16;
+    hex = hexchars[rem] + hex;
+    dec = dec / 16;
+}
+
+Console.Write("La conversion hexadecimale est "+hex);
